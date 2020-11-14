a0d711d1b5228c453ee568a1f17b6715134bb373
Mittie Adan <madan@mi-b5.msk.21-school.ru>
2020-11-14 10:52:24 +0300

    all delete

--- 02_bonus/filebeat/kibanawait.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-	sleep 1;
-	curl -sS kibana:5601 2>/dev/null && break;
-done;
-while [[ $(curl -sS kibana:5601/status) == 'Kibana server is not ready yet' ]]; do
-	sleep 1;
-done;
